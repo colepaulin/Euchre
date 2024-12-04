9523ae0f1fd0fbe5e9b578d87129baced24ee38f
Cole Paulin <cpaulin@mit.edu>
2024-12-04 15:03:25 -0500

    added PPOStrat

--- PPOStrategy.py
+++ PPOStrategy.py
@@ -5,7 +5,7 @@ from Card import Card
 from Player import Player
 from Team import Team
 
-class RandomStrategy(Strategy):
+class PPOStrategy(Strategy):
     """
     A simple strategy that makes all decisions randomly.
     This can serve as a baseline for comparing other strategies.
@@ -24,25 +24,19 @@ class RandomStrategy(Strategy):
                    teams: List[Team], 
                    faceUpCard, #: Card | None, 
                    biddingOrder: List[Player]) -> bool:
-        """
-        randomly decide to pass or play. 
-        :returns True if Play, false otherwise
-        """
-        return random.choice([True, False])
+        pass
 
 
     def discard(self, player: Player):
-        if player.cardsInHand:
-            card_to_discard = random.choice(player.cardsInHand)
-            player.cardsInHand.remove(card_to_discard)
+        pass
 
     def shouldGoAlone(self, player: Player, 
                       trumpSuit, 
                       teams: List[Team]) -> bool:
-        return random.choice([True, False])
+        pass
     
     def chooseTrump(self, player: Player):
-        return random.choice(['H','C','S','D'])
+        pass
 
     def playCard(self, player: Player, 
                  trumpSuit,
@@ -50,23 +44,6 @@ class RandomStrategy(Strategy):
                  teams: List[Team],
                  handHistory, 
                  trickHistory):
-        """
-        Randomly select a legal card to play from hand.
-        Must follow suit if possible.
-        """
-        if player.partner.isGoingAlone:
-            return None
-        # If a suit was led, must follow suit if possible
-        if leadSuit:
-            matching_cards = [card for card in player.cardsInHand if card.suit == leadSuit]
-            if matching_cards:
-                chosen_card = random.choice(matching_cards)
-                player.cardsInHand.remove(chosen_card)
-                return chosen_card
+        pass
         
-        # If no matching cards or no lead suit, can play any card
-        chosen_card = random.choice(player.cardsInHand)
-        player.cardsInHand.remove(chosen_card)
-        player.cardsPlayed.append(chosen_card)
-        return chosen_card
         
